d07feb79948e520c852684b1dc458db79732c2ba
AdriannnnO <95174036+AdriannnnO@users.noreply.github.com>
2022-09-19 12:39:18 +0200

    nie
    wiem

---
+++ www1lekcja/blog/blog/admin.py
@@ -0,0 +1,33 @@
+import imp
+from django.contrib import admin
+import django.contrib
+from blog.models import Answer, Question,Choice
+# models = django.apps.apps.get_models()
+
+# for model in models:
+#     try:
+#         admin.site.register(model)
+#     except admin.sites.AlreadyRegistered:
+#         pass
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date')
+    search_fields = ("question_text",)
+    list_filter = ("question_text",)
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('question','choice_text','votes')
+    search_fields = ("choice_text",)
+    list_filter = ("question","choice_text","votes",)
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('question','answer')
+    search_fields = ('question','answer')
+    list_filter = ('question',"answer")
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice,ChoiceAdmin)
+admin.site.register(Answer,AnswerAdmin)
