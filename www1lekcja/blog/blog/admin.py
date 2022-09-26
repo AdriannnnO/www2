c6bc30763a456156deb52b2c30ad31f0f4807535
AdriannnnO <95174036+AdriannnnO@users.noreply.github.com>
2022-09-26 13:36:07 +0200

    Update admin.py

--- www1lekcja/blog/blog/admin.py
+++ www1lekcja/blog/blog/admin.py
@@ -1,6 +1,6 @@
-import imp
+#import imp
 from django.contrib import admin
-import django.contrib
+#import django.contrib
 from blog.models import Answer, Question,Choice
 # models = django.apps.apps.get_models()
 
@@ -10,18 +10,18 @@ from blog.models import Answer, Question,Choice
 #     except admin.sites.AlreadyRegistered:
 #         pass
 
-class AuthorAdmin(admin.ModelAdmin):
-    pass
+# class AuthorAdmin(admin.ModelAdmin):
+#     pass
 
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('question_text', 'pub_date')
     search_fields = ("question_text",)
-    list_filter = ("question_text",)
+    list_filter = ("question_text",'pub_date')
 
 class ChoiceAdmin(admin.ModelAdmin):
     list_display = ('question','choice_text','votes')
     search_fields = ("choice_text",)
-    list_filter = ("question","choice_text","votes",)
+    list_filter = ("votes",)
 
 class AnswerAdmin(admin.ModelAdmin):
     list_display = ('question','answer')
